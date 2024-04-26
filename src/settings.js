3f72fee850ff74c284df3026a68abf748ba6a951
Jamnic98 <jamie@qudo.ai>
2024-04-26 21:16:01 +0100

    test

--- src/settings.js
+++ src/settings.js
@@ -3,4 +3,5 @@
 // 		? 'http://localhost:8000'
 // 		: process.env.PROD_SERVER_URL
 
-export const serverBaseURL = 'http://ec2-54-144-133-243.compute-1.amazonaws.com'
+export const serverBaseURL = 'https://ai-text-game.com'
+// export const serverBaseURL = 'http://localhost:8000'
